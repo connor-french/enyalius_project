d552afca71fcc2c39b643557a95793fe45972799
Connor French <confrench33@gmail.com>
2022-02-27 12:26:33 -0500

    first readme. Need to actually add stuff

--- dev/setup.R
+++ dev/setup.R
@@ -1,3 +1,6 @@
 # Helpful usethis functions for setting up documentation and other package features that will keep this easy going forward
 usethis::use_ccby_license()
-usethis::use_github()
+usethis::use_readme_rmd( open = FALSE )
+
+# for the end, in case other people contribute
+usethis::use_tidy_thanks()
