90882f41d82879f0c1d6cb841aeb5930a4504090
Connor French <confrench33@gmail.com>
2023-03-10 15:57:16 -0500

    finished first round of cluster threshold pcas

--- assembly/scripts/plot_ipyrad_pca.R
+++ assembly/scripts/plot_ipyrad_pca.R
@@ -1,3 +1,6 @@
+# functions to plot PCA output
+
+
 # function to plot the first 4 PCs for a PCA run by ipyrad
 # pca_obj needs to be output from ipyrad.analysis.pca
 # x and y are the plot axes and need to be the form "PC_1"
@@ -28,3 +31,31 @@ plot_pca <- function(pca_obj,
     labs(title = paste0("E. ", species_name)) +
     theme_bw()
 }
+
+
+# function to plot the cumulative variances of the first 8 PCs for each clustering threshold
+# pca_obj_list should be a named list of outputs from ipyrad's pca function, where the names are the cluster thresholds
+plot_cumvar <- function(pca_obj_list) {
+
+  # get the
+  variances <- vector(mode = "numeric", length = length(pca_obj_list))
+  for (i in 1:length(pca_obj_list)){
+    variances[[i]] <- sum(pca_obj_list[[i]]$variances[[1]][1:8])
+  }
+
+  # tibble of variances for plotting
+  vardf <- tibble(
+    clust_thresh = names(pca_obj_list),
+    cumvar = variances
+  )
+
+  p <- ggplot(vardf, aes(x = clust_thresh, y = cumvar)) +
+    geom_point(size = 3) +
+    labs(y = "Cumulative variance in first 8 PCs") +
+    theme_bw()
+
+  return(p)
+
+}
+
+
